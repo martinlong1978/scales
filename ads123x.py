950857f81a86250b985d982abc17213d5d42a53a
Martin Long <martin@longhome.co.uk>
2021-08-31 22:34:20 +0100

    Lots more user interface stuff

--- ads123x.py
+++ ads123x.py
@@ -3,16 +3,24 @@ import time
 
 buffersize = 10
 
+STATUS_WAITING = 0
 STATUS_OK = 0
-STATUS_TARE = 1
-STATUS_SETTLE = 2
+STATUS_SETTLE = 1
+STATUS_TARE = 2
+
+# this should allow handing of ADS1232 and ADS1234 boards. 
 
 class ADCBoard :
 
-    def __init__(self, clockpin, datapin):
+    def __init__(self, clockpin, datapin, powerpin, inputselect):
         self.clk = digitalio.DigitalInOut(clockpin)
         self.da = digitalio.DigitalInOut(datapin)
+        self.pwr = digitalio.DigitalInOut(powerpin)
+        self.islc = digitalio.DigitalInOut(inputselect)
+
         self.clk.direction = digitalio.Direction.OUTPUT
+        self.pwr.direction = digitalio.Direction.OUTPUT
+        self.islc.direction = digitalio.Direction.OUTPUT
 
     def readbit(self):
         self.clk.value = True
@@ -21,12 +29,14 @@ class ADCBoard :
         v = self.da.value
         return v
 
-    def readvalue(self):
+    def readvalue(self, input):
+        self.islc.value = input
         self.clk.value = False
+#        time.sleep(0.0000002)
         test = 0
-        while(test < 5):
+        while(test < 15):
             test = (test + 1) if self.da.value == False else 0
-            time.sleep(0.02)
+            time.sleep(0.002)
         val = 0
         for x in range(0,24):
             val = val * 2
@@ -39,14 +49,16 @@ class ADCBoard :
         return val
     
     def powerdown(self):
-        pass
+        self.pwr.value = False
 
     def powerup(self):
-        pass
+        self.pwr.value = True
+        self.pwr.value = False
+        self.pwr.value = True
 
 class LoadCell :
 
-    def __init__(self, board, calibration):
+    def __init__(self, board, calibration, input):
         self.ringbuffer = [0] * buffersize
         self.tmpringbuffer = [0] * buffersize
         self.pointer = 0
@@ -55,13 +67,14 @@ class LoadCell :
         self.avg = 0
         self.board = board
         self.tare = 0
+        self.input = input
 
 
     def pollweight(self):
         val = 16777215
         while val == 16777215 :
-            val = self.board.readvalue()
-        if abs(self.avg - val)  > (self.cal * 3) :
+            val = self.board.readvalue(self.input)
+        if self.cal > 1 and abs(self.avg - val)  > (self.cal * 3) :
             self.tmpringbuffer[self.tmppointer] = val
             self.tmppointer += 1 
             if(self.tmppointer >= buffersize):
@@ -83,8 +96,19 @@ class LoadCell :
                 self.tare = self.avg
         if(self.tare == 0):
             return 0, STATUS_TARE
-        return self.avg - self.tare, STATUS_OK
+        return (self.avg - self.tare) / self.cal, STATUS_OK
 
     def dotare(self):
         self.tare = 0
+
+    def begincal(self):
+        self.dotare()
+        self.cal = 1
+    
+    def calibrate(self, calreadings, refreadings, target):
+        print(f"{calreadings}, {refreadings}")
+        ratio = (calreadings[0] - calreadings[1])/(refreadings[1] - refreadings[0])
+        sum = calreadings[0] + (refreadings[0] * ratio)
+        self.cal = sum / target
+        print(f"Calibration set to {self.cal}")
 
--- code.py
+++ code.py
@@ -16,6 +16,7 @@ import array
 import countio
 from adafruit_bitmap_font import bitmap_font
 import ads123x
+import alarm
 #import traceback
 
 from adafruit_display_text import label
@@ -27,7 +28,6 @@ smallfont = terminalio.FONT #bitmap_font.load_font("/fonts/ArchivoNarrow-Bold-12
 
 def setdisplay():
     global display
-
     displayio.release_displays()
 
     # Use for I2C
@@ -47,6 +47,7 @@ class MultiTare :
 
     def __init__(self, loadcells):
         self.loadcells = loadcells
+        self.name = "Multple Tare"
 
 
     def init(self):
@@ -60,9 +61,9 @@ class MultiTare :
         self.tarecache = None
 
         #trs = [f' {x+1} ' for x in range(0, self.tares)]
-        self.tare_area = label.Label(smallfont, text='', color=0xFFFFFF, x=20, y=4)
-        self.text_area = label.Label(font, text='', color=0xFFFFFF, x=20, y=30)
-        self.status_area = label.Label(smallfont, text='', color=0xFFFFFF, x=20, y=56)
+        self.tare_area = label.Label(smallfont, text='', color=0xFFFFFF, x=10, y=4)
+        self.text_area = label.Label(font, text='', color=0xFFFFFF, x=10, y=30)
+        self.status_area = label.Label(smallfont, text='', color=0xFFFFFF, x=10, y=56)
         splash.append(self.text_area)
         splash.append(self.tare_area)
         splash.append(self.status_area)
@@ -88,17 +89,16 @@ class MultiTare :
         tarelist = ''.join(trs)
         self.tare_area.text = tarelist
 
+    def gettarevalue(self) :
+        if self.tarecache == None :
+            self.tarecache = sum(self.tarevalues) - self.tarevalues[self.active]
+        return self.tarecache
 
     def showweight(self, weight, status):
         self.status = status
         if status == ads123x.STATUS_OK:
             self.weight = round(weight, 1)
         self.refresh()
-
-    def gettarevalue(self) :
-        if self.tarecache == None :
-            self.tarecache = sum(self.tarevalues) - self.tarevalues[self.active]
-        return self.tarecache
 
     def btn_a(self) :
         self.tarevalues[self.active] = self.weight - self.gettarevalue()
@@ -110,8 +110,112 @@ class MultiTare :
         self.tarevalues = [0.0] * self.tares
         for x in loadcells : x.dotare()
 
+class Calibrate :
+    def __init__(self, loadcells):
+        self.loadcells = loadcells
+        self.name = "Calibrate"
+
+    def init(self):
+        splash = displayio.Group()
+        display.show(splash)
+        self.stage = 0
+
+        self.status_area = label.Label(smallfont, text='Resetting Tare', color=0xFFFFFF, x=20, y=56)
+        splash.append(self.status_area)
+        for x in self.loadcells : x.begincal()
+        #self.refresh()
+
+    def nudge(self):
+        if(self.stage == 0):
+            self.stage = 1
+            self.status_area.text = "Place 500 in pos A"
+        if(self.stage == 2):
+            self.stage = 3
+            self.first = [x.avg - x.tare for x in self.loadcells]
+            self.status_area.text = "Place 500 in pos B"
+        if(self.stage == 4):
+            self.stage = 5
+            self.second = [x.avg - x.tare for x in self.loadcells]
+            for x in range(0,2) : self.loadcells[x].calibrate([self.first[x], self.second[x]], [self.first[1-x], self.second[1-x]], 500)
+            self.status_area.text = "Calibrated"
+
+    def showweight(self, weight, status):
+        if status == ads123x.STATUS_OK :
+            self.status = status
+            self.nudge()
+        #self.refresh()
+
+    def btn_a(self) :
+        if(self.stage == 1):
+            self.stage = 2 
+            self.status_area.text = "Waiting"
+        if(self.stage == 3):
+            self.stage = 4
+            self.status_area.text = "Waiting"
+
+    def btn_b(self) :
+        pass
+
+class Menu :
+    def __init__(self, loadcells):
+        self.loadcells = loadcells
+
+    def init(self):
+        global mode
+        global prevmode
+        splash = displayio.Group()
+        display.show(splash)
+
+        self.index = 0
+        
+        self.lines = [label.Label(smallfont, text=x.name, color=0xFFFFFF, x = 8, y = 8 + i * 14) for i,x in enumerate(modes)]
+        for x in self.lines : splash.append(x)
+        self.update()
+
+    def update(self):
+        for i,x in enumerate(self.lines) : 
+            if i == self.index : 
+                x.color = 0x000000
+                x.background_color = 0xFFFFFF
+            else :
+                x.color = 0xFFFFFF
+                x.background_color = 0x000000
+
+    def showweight(self, weight, status):
+        pass
+
+    def btn_a(self) :
+        self.index = (self.index + 1) % len(modes)
+        self.update()
+
+    def btn_b(self) :
+        global mode
+        global prevmode
+        mode = modes[self.index]
+        prevmode = self
+        mode.init()
+
+
 def switchmode():
-    pass
+    global mode
+    global prevmode
+    m = prevmode
+    prevmode = mode
+    mode = m
+    mode.init() 
+    #poweroff()
+
+def poweroff():
+    adc.powerdown()
+    btn_a.deinit()
+    btn_b.deinit()
+    btn_c.deinit()
+    pin_alarm1 = alarm.pin.PinAlarm(pin=board.D5, value=False, pull=True)
+
+    alarm.exit_and_deep_sleep_until_alarms(pin_alarm1) #, pin_alarm2, pin_alarm3)
+
+
+
 
 btn_a = countio.Counter(board.D5)
 btn_b = countio.Counter(board.D21)
@@ -119,23 +223,27 @@ btn_c = countio.Counter(board.D20)
 
 setdisplay()
 
-adc = ads123x.ADCBoard(board.D18, board.D19)
-loadcells = [ads123x.LoadCell(adc, 22)]
+adc = ads123x.ADCBoard(board.D18, board.D19, board.D17, board.D16)
+adc.powerup()
 
-mode = MultiTare(loadcells)
+loadcells = [ads123x.LoadCell(adc, 20.592, False), ads123x.LoadCell(adc, 20.592, True)]
+
+modes = [MultiTare(loadcells), Calibrate(loadcells)]
+menu = Menu(loadcells)
+mode = modes[0]
 mode.init()
-
+prevmode = menu
 
 while True: 
-    wt, status = loadcells[0].pollweight()
+    vals =  [x.pollweight() for x in loadcells]
+    wt = sum([x[0] for x in vals])
+    status = max([x[1] for x in vals])
     mode.showweight(wt, status)
     if(btn_a.count > 0) :
-        for i in range(0, btn_a.count):
-            mode.btn_a()
+        mode.btn_a()
         btn_a.reset()
     if(btn_b.count > 0) :
-        for i in range(0, btn_b.count):
-            mode.btn_b()
+        mode.btn_b()
         btn_b.reset()
     if(btn_c.count > 0) :
         btn_c.reset()
